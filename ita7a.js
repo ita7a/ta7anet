b61ac2c5bae5e1035988baf4c75dd5029a978455
ita7a <57022144+ita7a@users.noreply.github.com>
2020-11-17 04:35:37 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,16 @@
+try{$('.u-msg.dots').removeClass('dots');$(".ubnr").remove();$(`<link rel="stylesheet" href="https://rawcdn.githack.com/ita7a/chatt7/bd07169ece50f9e9c09a8618ff90355049d350c9/chat.css"></style>`).insertBefore("body");$('.hand.nosel.fl.uzr.border').css({'margin':'1px 0',borderRight:'1px solid #EDEDED',borderLeft:'1px solid #adadad',borderTop:'1px solid #EDEDED',borderBottom:'1px solid #EDEDED',paddingTop:'3px',borderRadius:'5px'});$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" " id="ita7a" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> iTa7a.CoM | شات التحلية  </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');$('.room.borderg.hand.nosel.fl').css({'margin':'1px 0',borderRight:'3px solid #EDEDED',borderLeft:'3px solid #EDEDED',borderTop:'1px solid #EDEDED',borderBottom:'1px solid #EDEDED',paddingTop:'3px',borderRadius:'5px'});$('.fr.borderg a').text('دوك موبايل')
+$("a.label.label-primary.fl > img.fl").replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="Muhanad" name="FirstName" value="FM اذاعة التحلية ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://curiosity.shoutca.st:6035/ ؛"><a href="http://curiosity.shoutca.st:6035/ ؛</a></audio>
+
+<center></center></center>
+
+</div>`);
+
+$('#d0').append(`<label title="الاذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`);;if(location.protocol!='https:'){location.protocol='https:';}
+if(getUrlParameter('enter')!=null){location.href='/';}
+AddMsg_=AddMsg;AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String))
